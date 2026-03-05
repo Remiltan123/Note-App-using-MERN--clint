9e9fe1ab9382c9d6bc53078935f81041f909e517
Dhanos_1119 <dhanoshiganratnarajah2001@gmail.com>
2026-03-04 23:48:23 -0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+// import authRoutes from "./routes/auth.js";
+// import notesRoutes from "./routes/notes.js";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+// app.use("/api/users", authRoutes);
+// app.use("/api/notes", notesRoutes);
+
+console.log("Connecting to MongoDB...");
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server succesfully running at port ${PORT}`);
+});
