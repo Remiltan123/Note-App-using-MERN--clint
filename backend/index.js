b25ccd92946f639b50a1dcb7dad94ace7eac7b25
Dhanos_1119 <dhanoshiganratnarajah2001@gmail.com>
2026-03-05 00:07:48 -0800

    Add the model and auth service

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-// import authRoutes from "./routes/auth.js";
-// import notesRoutes from "./routes/notes.js";
+import authRoutes from "./routes/authService.js";
+import notesRoutes from "./routes/notesService.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-// app.use("/api/users", authRoutes);
-// app.use("/api/notes", notesRoutes);
+app.use("/api/users", authRoutes);
+app.use("/api/notes", notesRoutes);
 
 console.log("Connecting to MongoDB...");
 connectDB();
